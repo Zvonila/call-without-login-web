de34219f7f1027f6b5474b608f537c8dc6519087
Vladislav Samkov <vladislav.kori.work@gmail.com>
2025-11-09 11:16:01 +0500

    test different configuration

--- src/entities/core/core.ts
+++ src/entities/core/core.ts
@@ -26,13 +26,14 @@ export class ZvonilaCore extends EventEmitter {
         const pc = new RTCPeerConnection({
             iceServers: [
                 { urls: import.meta.env.VITE_STUN_URL },
-                { urls: "stun:stun4.l.google.com:19302" },
+                // { urls: "stun:stun4.l.google.com:19302" },
                 {
                     urls: import.meta.env.VITE_TURN_URL,
                     username: import.meta.env.VITE_TURN_USERNAME,
                     credential: import.meta.env.VITE_TURN_CREDENTIAL
                 }
             ],
+            // iceTransportPolicy: "relay"
         });
 
         pc.onicegatheringstatechange = () => {
